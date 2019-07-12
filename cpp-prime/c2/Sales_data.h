84d697cfc967d43caf1aa053e13c66e195743b8a
XiaochenCui <jcnlcxc.new@gmail.com>
2019-07-12 17:49:05 +0800

    update

---
+++ cpp-prime/c2/Sales_data.h
@@ -0,0 +1,15 @@
+//
+// Created by xiaochen on 2019-07-12.
+//
+
+#include <string>
+
+#ifndef CPP_STUDY_SALES_DATA_H
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+#define CPP_STUDY_SALES_DATA_H
+
+#endif //CPP_STUDY_SALES_DATA_H
---
+++ cpp-prime/c2/ex2-39.cpp
@@ -0,0 +1,9 @@
+//
+// Created by xiaochen on 2019-07-12.
+//
+
+struct Foo {}
+int main()
+{
+    return 0;
+}
