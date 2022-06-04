a2be2d87ead247fa3bf7eccc4e4d5d7fca2ef0f6
XiaochenCui <jcnlcxc.new@gmail.com>
2022-06-04 14:03:02 +0800

    update

--- src/pointer_and_reference/pointer_to_pointer_arg.cpp
+++ src/pointer_and_reference/pointer_to_pointer_arg.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 
-int main() { std::cout << "Hello, World!" << std::endl; }
+struct Foo {
+  int a;
+};
+
+void receive_ref_to_pointer(int depth, Foo*& foo) {
+  if (depth > 3) {
+    return;
+  }
+  std::cout << "receive_ref_to_pointer: " << depth << " " << foo << std::endl;
+  receive_ref_to_pointer(depth + 1, foo);
+}
+
+void receive_pointer_to_pointer(int depth, Foo** foo) {
+  if (depth > 3) {
+    return;
+  }
+  std::cout << "receive_pointer_to_pointer: " << depth << " " << *foo << std::endl;
+  receive_pointer_to_pointer(depth + 1, foo);
+}
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  Foo* new_foo = nullptr;
+  receive_ref_to_pointer(0, new_foo);
+  receive_pointer_to_pointer(0, &new_foo);
+}
